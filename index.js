3b7b451f39c6b2d22f1388903255a42e8a45749b
vitti10 <vitti_22_12@hotmail.com>
2023-05-30 19:56:31 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const miBoton = document.getElementById("miBoton");
+miBoton.addEventListener("click",function handleClick(){
+    alert("¡Haz echo clic en el boton!");
+});
+
+function handleClick(){alert("¡Hola! has echo clic en el Boton.");}
+
+
+
+
+
